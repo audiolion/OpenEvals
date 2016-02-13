edd25fc62e119c00e49f825cb118be9016b4c250
zujko <zujko@csh.rit.edu>
2016-02-13 18:51:56 -0500

    Moved courses app to main app

--- OpenEvals/settings.py
+++ OpenEvals/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses.apps.CoursesConfig',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- OpenEvals/urls.py
+++ OpenEvals/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^', include('courses.urls'))
+    url(r'^', include('main.urls'))
 ]
