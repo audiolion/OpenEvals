23eb698b3af197f010420cfaf5d8c6b08eab2937
Nathan Castle <nathan.castle@outlook.com>
2016-02-15 00:47:58 -0500

    Started/improved pages
    Implemented routing, basic views (nothing attached to the model yet)

--- main/views.py
+++ main/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
     return render(request, 'main/about.html')
+    
+def search(request):
+    return render(request, 'main/search.html')
+
+def professor(request, lastname, firstname):
+    return render_to_response('main/professor.html', {'firstname': firstname,'lastname':lastname})
+    
+def course(request, coursecode, coursenumber):
+    return render_to_response('main/course.html', {'coursecode': coursecode,'coursenumber':coursenumber})
