4d43a34368613d0f7b5ae6a65e1784f6396c80a4
aaron13michael <ams8560@rit.edu>
2016-02-17 15:56:40 -0500

    added CLASS_EVAL_QUESTIONS and CLASS_EVAL_RESULTS to dev database for testing

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from .models import *
 
 def index(request):
     return render(request, 'main/index.html')
@@ -10,14 +11,18 @@ def about(request):
 def search(request):
     if request.method == 'POST':
         data = request.POST["search"]
-        result = list()
-        result.append("This is for testing")
-        result.append("You searched for: " + data)
+        result = EvalResults.objects.filter(instr_first_name__in=data, class_code__in=data)
+
         return render(request, 'main/search.html', {'result': result})
     else:
         return render(request, 'main/search.html')
 
 def professor(request, lastname, firstname):
+    prof = EvalResults.objects.filter(instr_last_name=lastname,
+                                      instr_first_name=firstname)
+    questions = EvalQuestions.objects.all()
+
+
     return render_to_response('main/professor.html', {'firstname': firstname,'lastname':lastname})
     
 def course(request, coursecode, coursenumber):
