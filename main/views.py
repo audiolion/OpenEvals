b550a4c439e9f54081a8834df289f8930459805e
zujko <zujko@csh.rit.edu>
2016-02-19 16:53:01 -0500

    WIP Average all professor question ratings and display them in the table

--- main/views.py
+++ main/views.py
@@ -20,10 +20,28 @@ def search(request):
 def professor(request, lastname, firstname):
     prof = EvalResults.objects.filter(instr_last_name=lastname,
                                       instr_first_name=firstname)
+    prof_size = len(prof)
+
     questions = EvalQuestions.objects.all()
+    q_ratings = [0] * 10
 
+    # Average question ratings
+    for professor in prof:
+        q_ratings[0] += professor.q1_average
+        q_ratings[1] += professor.q2_average
+        q_ratings[2] += professor.q3_average
+        q_ratings[3] += professor.q4_average
+        q_ratings[4] += professor.q5_average
+        q_ratings[5] += professor.q6_average
+        q_ratings[6] += professor.q7_average
+        q_ratings[7] += professor.q8_average
+        q_ratings[8] += professor.q9_average
+        q_ratings[9] += professor.q10_average
 
-    return render_to_response('main/professor.html', {'firstname': firstname,'lastname':lastname})
+    for index in range(10):
+        q_ratings[index] /= prof_size 
+
+    return render_to_response('main/professor.html', {'firstname': firstname,'lastname':lastname, 'questions': questions, 'ratings': q_ratings})
     
 def course(request, coursecode, coursenumber):
     return render_to_response('main/course.html', {'coursecode': coursecode,'coursenumber':coursenumber})
