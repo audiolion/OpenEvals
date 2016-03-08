c55f480c48f3b511be3bc3a220bcb624ba031c63
Nathan Castle <nathan.castle@outlook.com>
2016-03-08 00:59:08 -0500

    404 & front page improvements

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_list_or_404, render
 from django.shortcuts import render_to_response
+from django.template  import RequestContext
 from django.db.models import Q
 from functools import reduce
 from itertools import chain
@@ -35,11 +36,11 @@ def professor(request, lastname, firstname):
     lastname = lastname.title()
     firstname = firstname.title()
     prof = get_list_or_404(EvalResults, instr_last_name=lastname, instr_first_name=firstname)
-    
+
     prof_size = len(prof)
 
     questions = EvalQuestions.objects.all()
-    
+
     q_ratings = [0] * 10
 
     # Average question ratings
@@ -60,18 +61,23 @@ def professor(request, lastname, firstname):
     # Removes duplicate classes that the professor teaches
     seen = set()
     courses = [x for x in prof if x.class_code not in seen and not seen.add(x.class_code)]
-    
+
     # Get all results for classes this professor teaches
     similar_profs_query = reduce(lambda q,course: q|Q(class_code=course.class_code), courses, Q())
     sim_profs = EvalResults.objects.filter(similar_profs_query).exclude(instr_first_name=firstname)
     # Removes duplicate professors
     prof_seen = set()
     similar_profs = [x for x in sim_profs if x.instr_full_name not in seen and not seen.add(x.instr_full_name)]
-    
+
     return render(request, 'main/professor.html', {'firstname': firstname,'lastname':lastname, 'questions': questions, 'ratings': q_ratings, 'courses': courses,'sim_profs': similar_profs})
-    
+
 def course(request, coursecode, coursenumber):
     return render_to_response('main/course.html', {'coursecode': coursecode,'coursenumber':coursenumber})
+
+def handler404(request):
+    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 
 '''
 HELPER METHODS
