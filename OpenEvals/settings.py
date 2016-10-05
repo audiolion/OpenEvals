d34e5870f364df8516b2fc7d710901629fad627a
Juliana Kroll <juliana@Starlight.local>
2016-10-05 12:48:22 -0400

    Development setup. Search by class description name. Search bar persists on each page. Landing view for search.

--- OpenEvals/settings.py
+++ OpenEvals/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 from OpenEvals import secrets
 import os
+import ldap
+from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +27,8 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['openevals.rit.edu','evals.rit.edu']
 
 
 # Application definition
@@ -56,11 +59,7 @@ STATICFILES_FINDERS = (
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_PRECOMPILER_COMPILERS = (
     ('static_precompiler.compilers.LESS', {
@@ -105,47 +104,53 @@ WSGI_APPLICATION = 'OpenEvals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+DATABASE_ROUTERS = ['main.models.CustomRouter']
+
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': secrets.NAME,
+        #'NAME': '/var/www/evals/user',
+        'NAME': 'user.db',
         #'USER': secrets.USER,
         #'PASSWORD': secrets.PASSWORD,
         #'HOST': secrets.HOST,
         #'PORT': secrets.PORT,
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'dev'
     },
-    #'oracle' :{
-        #'ENGINE': 'django.db.backends.oracle',
-        #'NAME': secrets.ORACLE_NAME,
-        #'USER': secrets.ORACLE_USER,
-        #'PASSWORD': secrets.ORACLE_PASSWORD,
-        #'HOST': secrets.ORACLE_HOST,
-        #'PORT': secrets.ORACLE_PORT,
-    #}
+#    'oracle': {
+#        'ENGINE': 'django.db.backends.oracle',
+#        'NAME': secrets.ORACLE_NAME,
+#        'USER': secrets.ORACLE_USER,
+#        'PASSWORD': secrets.ORACLE_PASSWORD,
+#        'HOST': secrets.ORACLE_HOST,
+#        'PORT': secrets.ORACLE_PORT,
+#    }
 
 }
 
+# LDAP Config
+AUTH_LDAP_SERVER_URI = "ldaps://ldap.rit.edu"
 
-# Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+AUTH_LDAP_BIND_DN = "uid=" + secrets.LDAP_USER + ",ou=People,dc=rit,dc=edu"
+AUTH_LDAP_BIND_PASSWORD = secrets.LDAP_PASSWORD
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=rit,dc=edu", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=Groups,dc=rit,dc=edu", ldap.SCOPE_SUBTREE, "(objectClass=groupOfNames)")
+AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+#Require student, prevent others
+AUTH_LDAP_REQUIRE_GROUP = "cn=student,ou=Groups,dc=rit,dc=edu"
+AUTH_LDAP_DENY_GROUP = "cn=staff,ou=Groups,dc=rit,dc=edu"
+AUTH_LDAP_DENY_GROUP = "cn=faculty,ou=Groups,dc=rit,dc=edu"
+AUTH_LDAP_DENY_GROUP = "cn=studemp,ou=Groups,dc=rit,dc=edu"
 
+#AUTH_LDAP_USER_ATTR_MAP = {
+#    "first_name": "givenName",
+#    "last_name": "sn",
+#}
+
+AUTHENTICATION_BACKENDS = ('django_auth_ldap.backend.LDAPBackend', 'django.contrib.auth.backends.ModelBackend')
+
+LOGIN_REDIRECT_URL = '/search/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
@@ -161,7 +166,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_list_or_404, render
 from django.shortcuts import render_to_response
-from django.template  import RequestContext
+from django.template import RequestContext
 from django.db.models import Q
 from functools import reduce
 from itertools import chain
@@ -10,36 +11,68 @@ from .models import *
 from .Professor import *
 from .Course import *
 from .forms import SearchForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 import json
+
 
 def index(request):
     return render(request, 'main/index.html')
 
+
 def about(request):
     return render(request, 'main/about.html')
 
+
+def login_view(request):
+    if len(request.POST) > 0:
+        usern = request.POST.get('Username')
+        passwd = request.POST.get('Password')
+        user = authenticate(username=usern, password=passwd)
+        if user is not None:
+            login(request, user)
+            if (request.GET.get['next']):
+                return redirect(request.GET.get('next'))
+            return redirect('/search/')
+        else:
+            return render(request, 'main/login.html')
+    return render(request, 'main/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+# @login_required
 def search(request, searchQ):
     if request.method == 'GET' and len(request.GET) > 0:
         form = SearchForm(request.GET)
-        if form.is_valid():
+        if (form.is_valid()):
+            if form.cleaned_data['search'].lower() == '':  # csrf breaks form length test in prod
+                return render(request, 'main/search.html', {'search_form': form})
             searchData = form.cleaned_data['search'].lower()
             data = searchData.split()
 
-        foundProfs = professorsSearch(searchData)
-        foundCourses = coursesSearch(data, searchData)
-
-        return render(request, 'main/search.html', {'professors': foundProfs, "courses" : foundCourses, 'search_form' : form },)
+            foundProfs = professorsSearch(searchData)
+            foundCourses = coursesSearch(data, searchData)
+            return render(request, 'main/search.html',
+                          {'professors': foundProfs, "courses": foundCourses, 'search_form': form}, )
+        return render(request, 'main/search.html', {'search_form': form})
     else:
         form = SearchForm()
-        if(searchQ != ""):
-            #This is solely for running a search via the common course link
-            #in a professor tile. Searching via a url not yet supported
+        if (searchQ != ""):
+            # This is solely for running a search via the common course link
+            # in a professor tile. Searching via a url not yet supported
             foundCourses = coursesSearch([searchQ], searchQ)
-            return render(request, 'main/search.html', {"courses" : foundCourses, 'search_form' : form })
+            return render(request, 'main/search.html', {"courses": foundCourses, 'search_form': form})
         else:
-            return render(request, 'main/search.html', {'search_form' : form })
+            return render(request, 'main/search.html', {'search_form': form})
 
+
+# @login_required
 def professor(request, lastname, firstname):
+    form = SearchForm()
     lastname = lastname.title()
     firstname = firstname.title()
     prof = get_list_or_404(EvalResults, instr_last_name=lastname, instr_first_name=firstname)
@@ -58,47 +91,55 @@ def professor(request, lastname, firstname):
         q_ratings[3] += professor.q4_average
         q_ratings[4] += professor.q5_average
         q_ratings[5] += professor.q6_average
-        q_ratings[6] += professor.q7_average
-        q_ratings[7] += professor.q8_average
-        q_ratings[8] += professor.q9_average
-        q_ratings[9] += professor.q10_average
-    for index in range(10):
+        # q_ratings[6] += professor.q7_average
+        # q_ratings[7] += professor.q8_average
+        # q_ratings[8] += professor.q9_average
+        # q_ratings[9] += professor.q10_average
+    for index in range(6):
         q_ratings[index] = round(q_ratings[index] / prof_size, 2)
 
     # Removes duplicate classes that the professor teaches
     seen = set()
-    courses = [x for x in prof if x.class_code not in seen and not seen.add(x.class_code)]
+    courses = [x for x in prof if
+               x.class_subj + x.class_cat_nbr not in seen and not seen.add(x.class_subj + x.class_cat_nbr)]
 
     # Get all results for classes this professor teaches
-    similar_profs_query = reduce(lambda q,course: q|Q(class_code=course.class_code), courses, Q())
+    similar_profs_query = reduce(lambda q, course: q | Q(class_code=course.class_code), courses, Q())
     sim_profs = EvalResults.objects.filter(similar_profs_query).exclude(instr_first_name=firstname)
-    
+
     # Removes duplicate professors
     prof_seen = set()
     similar_profs = [x for x in sim_profs if x.instr_full_name not in seen and not seen.add(x.instr_full_name)]
-   
+
     # Creates list of Course objects
     classes = list()
     for course in courses:
-        classes.append(create_course(course,firstname,lastname))
+        classes.append(create_course(course, firstname, lastname))
 
-    return render(request, 'main/professor.html', {'firstname': firstname,'lastname':lastname, 'questions': questions, 'ratings': q_ratings, 'courses': classes,'sim_profs': similar_profs})
+    return render(request, 'main/professor.html',
+                  {'search_form': form, 'firstname': firstname, 'lastname': lastname, 'questions': questions,
+                   'ratings': q_ratings, 'courses': prof, 'sim_profs': similar_profs})
+
 
 def create_course(course, fname, lname):
-    c = Course(course.class_code,course.class_subj,course.class_number)
-    profs = EvalResults.objects.filter(class_subj=course.class_subj, class_number=course.class_number).exclude(instr_last_name=lname)
-    
+    c = Course(course.class_code, course.class_subj, course.class_cat_nbr)
+    profs = EvalResults.objects.filter(class_subj=course.class_subj, class_cat_nbr=course.class_cat_nbr).exclude(
+        instr_last_name=lname)
     for prof in profs:
         p = Professor(prof.instr_first_name, prof.instr_last_name)
         c.addProfessor(p)
-        c.numSections = EvalResults.objects.filter(class_subj=course.class_subj, class_number=course.class_number).count() 
-    
+        c.numSections = EvalResults.objects.filter(class_subj=course.class_subj,
+                                                   class_cat_nbr=course.class_cat_nbr).count()
+
     return c
-    
-def course(request, coursecode, coursenumber):
-    #coursecode = coursecode.title()
-    #coursenumber = coursenumber.title()
-    sections = get_list_or_404(EvalResults, class_subj=coursecode, class_number=coursenumber)
+
+
+# @login_required
+def course(request, subj, classcatnbr):
+    form = SearchForm()
+    # coursecode = coursecode.title()
+    # coursenumber = coursenumber.title()
+    sections = get_list_or_404(EvalResults, class_subj=subj, class_cat_nbr=classcatnbr)
 
     sections_size = len(sections)
 
@@ -113,62 +154,70 @@ def course(request, coursecode, coursenumber):
         q_ratings[3] += courseScores.q4_average
         q_ratings[4] += courseScores.q5_average
         q_ratings[5] += courseScores.q6_average
-        q_ratings[6] += courseScores.q7_average
-        q_ratings[7] += courseScores.q8_average
-        q_ratings[8] += courseScores.q9_average
-        q_ratings[9] += courseScores.q10_average
+        # q_ratings[6] += courseScores.q7_average
+        # q_ratings[7] += courseScores.q8_average
+        # q_ratings[8] += courseScores.q9_average
+        # q_ratings[9] += courseScores.q10_average
 
-    for index in range(10):
+    for index in range(6):
         q_ratings[index] = round(q_ratings[index] / sections_size, 2)
 
-    return render_to_response('main/course.html', {'coursecode': coursecode,'coursenumber':coursenumber, 'questions': questions, 'ratings': q_ratings})
+    return render_to_response('main/course.html',
+                              {'search_form': form, 'subj': subj, 'classcatnbr': classcatnbr, 'questions': questions,
+                               'ratings': q_ratings, 'sections': sections})
+
 
 def handler404(request):
     response = render_to_response('404.html', {}, context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
+
 '''
 HELPER METHODS
 '''
+
+
 def professorsSearch(searchPOST):
     dataObjects = EvalResults.objects.all()
-    startsFull = dataObjects.filter(instr_full_name__istartswith=searchPOST) #search by first name
-    startsL = dataObjects.filter(instr_last_name__istartswith=searchPOST) #search by beginning of last name
-    startsF = dataObjects.filter(instr_first_name__istartswith=searchPOST) #search by beginning of first name
-    #combine results from search methods above
+    startsFull = dataObjects.filter(instr_full_name__istartswith=searchPOST)  # search by first name
+    startsL = dataObjects.filter(instr_last_name__istartswith=searchPOST)  # search by beginning of last name
+    startsF = dataObjects.filter(instr_first_name__istartswith=searchPOST)  # search by beginning of first name
+    # combine results from search methods above
     results = startsFull | startsL | startsF
 
     foundProfs = set()
-    #sets to prevent duplicate results
+    # sets to prevent duplicate results
     seenP = set()
     for result in results:
         seenC = set()
         if result.instr_full_name not in seenP:
             seenP.add(result.instr_full_name)
             p = Professor(result.instr_first_name, result.instr_last_name)
-            #Find courses that this professor teaches
+            # Find courses that this professor teaches
             myCourse = results.filter(instr_first_name=result.instr_first_name,
                                       instr_last_name=result.instr_last_name)
             for c in myCourse:
                 if c.class_desc not in seenC:
-                    seenC.add(c.class_code)
-                    p.addCourse(Course(c.class_code, c.class_subj, str(c.class_number)))
+                    seenC.add(c.class_subj)
+                    p.addCourse(Course(c.class_subj, c.class_subj, str(c.class_cat_nbr), c.class_desc))
 
             p.setCommonCourse()
             foundProfs.add(p)
     return foundProfs
 
+
 def coursesSearch(searchQuery, searchPOST):
-    seenC = set() #prevent duplicate tiles
+    seenC = set()  # prevent duplicate tiles
     dataObjects = EvalResults.objects.all()
-    fullName = dataObjects.filter(class_code__istartswith=searchPOST) #search by readable name
-    #separate lists because django throws a value error if you try to
-    #lookup an integer database field in a list containing strings and vice versa
+    fullName = dataObjects.filter(class_code__istartswith=searchPOST)  # search by readable name
+    descName = dataObjects.filter(class_desc__contains=searchPOST)
+    # separate lists because django throws a value error if you try to
+    # lookup an integer database field in a list containing strings and vice versa
     subjects = list()
     numbers = list()
-    #split up results in course-number format into a separate
-    #list that can be used for querying the data
+    # split up results in course-number format into a separate
+    # list that can be used for querying the data
     for item in searchQuery:
         if '-' in item:
             subjectNumber = item.split('-')
@@ -179,27 +228,26 @@ def coursesSearch(searchQuery, searchPOST):
                 except ValueError:
                     continue
 
-    subjNum = dataObjects.filter(class_subj__in=subjects, class_number__in=numbers) #search by subject-number
+    subjNum = dataObjects.filter(class_subj__in=subjects, class_number__in=numbers)  # search by subject-number
     if not subjNum.exists():
         subjNum = dataObjects.filter(class_subj__istartswith=searchPOST)
 
-    #combine results from above search method
-    results = fullName | subjNum
+    # combine results from above search method
+    results = fullName | subjNum | descName
     foundCourses = list()
     for result in results:
         seenP = set()
-        if result.class_code not in seenC:
-            seenC.add(result.class_code)
-            course = Course(result.class_code, result.class_subj, result.class_number)
-            #calculate how many sections there are
-            allSections = results.filter(class_subj=result.class_subj, class_number=result.class_number)
+        if result.class_subj + result.class_cat_nbr not in seenC:
+            seenC.add(result.class_subj + result.class_cat_nbr)
+            course = Course(result.class_code, result.class_subj, result.class_cat_nbr, result.class_desc)
+            # calculate how many sections there are
+            allSections = results.filter(class_subj=result.class_subj, class_cat_nbr=result.class_cat_nbr)
             course.numSections = allSections.count()
-            #Find campuses course is offered in
+            # Find campuses course is offered in
             for section in allSections:
-                course.campus.add(section.campus)
-            #Find professors who teach this course
+                course.addCampus(section.campus)
             myProfessor = results.filter(class_subj=result.class_subj,
-                                         class_number=result.class_number)
+                                         class_cat_nbr=result.class_cat_nbr)
             for prof in myProfessor:
                 if prof.instr_full_name not in seenP:
                     seenP.add(result.instr_full_name)
@@ -207,36 +255,41 @@ def coursesSearch(searchQuery, searchPOST):
             foundCourses.append(course)
     return foundCourses
 
+
 '''
 AUTOCOMPLETE VIEW
 '''
+
+
 def get_results(request):
     if request.is_ajax():
         currSearch = request.GET['term']
-        profs = EvalResults.objects.filter(instr_full_name__icontains=currSearch)[:3]
-        courses = EvalResults.objects.filter(class_code__istartswith=currSearch)[:3]
+        profs = EvalResults.objects.filter(instr_full_name__icontains=currSearch)[:10]
+        courses = EvalResults.objects.filter(class_code__istartswith=currSearch)[:10]
         if not courses.exists():
-            courses = EvalResults.objects.filter(class_subj__istartswith=currSearch)[:3]
+            courses = EvalResults.objects.filter(class_subj__istartswith=currSearch)[:10]
+        if not courses.exists():
+            courses = EvalResults.objects.filter(class_desc__icontains=currSearch)[:10]
         foundC = []
         foundP = []
-        #sets to prevent duplicate results
+        # sets to prevent duplicate results
         seenP = set()
         seenC = set()
         for result in profs:
             if result.instr_full_name not in seenP:
                 seenP.add(result.instr_full_name)
                 result_json = {}
-                result_json['id'] = result.id
+                result_json['id'] = result.instr_full_name
                 result_json['label'] = result.instr_full_name
                 result_json['value'] = result.instr_full_name
                 foundP.append(result_json)
         for result in courses:
-            if result.class_code not in seenC:
-                seenC.add(result.class_code)
+            if result.class_subj + result.class_cat_nbr not in seenC:
+                seenC.add(result.class_subj + result.class_cat_nbr)
                 result_json = {}
-                result_json['id'] = result.id
-                result_json['label'] = result.class_code
-                result_json['value'] = result.class_code
+                result_json['id'] = result.class_subj + result.class_cat_nbr
+                result_json['label'] = result.class_subj + " - " + result.class_cat_nbr + " " + result.class_desc
+                result_json['value'] = result.class_subj + result.class_cat_nbr
                 foundC.append(result_json)
 
         found = foundC + foundP
@@ -245,3 +298,10 @@ def get_results(request):
         data = 'fail'
     mimetype = 'application/json'
     return HttpResponse(data, mimetype)
+
+
+def handler500(request):
+    response = render_to_response('main/500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
