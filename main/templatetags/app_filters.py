15463e35626e381356d868583be90b54b3fc4883
Juliana Kroll <juliana@Starlight.wireless.rit.edu>
2016-11-02 00:04:17 -0400

    Added color scheme for ratings.

--- main/templatetags/app_filters.py
+++ main/templatetags/app_filters.py
@@ -9,3 +9,14 @@ def convert_100(value):
 @register.filter(name='convert_10')
 def convert_10(value):
     return str(round(float(value-1)*float(2.25), 2))
+
+@register.filter(name='color_rate')
+def color_rate(value):
+    temp = 1;
+    if value > 2:
+        temp = 2
+    if value > 3:
+        temp = 3
+    if value > 4:
+        temp = 4
+    return str("color-scale-rate-" + str(temp))
